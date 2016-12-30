db41700239f5427d59bb0863d801274acd7e5b04
pranavgadekar <ppgadekar.92@gmail.com>
2016-12-30 02:10:20 -0500

    Made request secure

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ jQuery( function( $ ) {
       $( '#get-another-quote-button' ).on( 'click', function ( e ) {
         e.preventDefault();
         $.ajax( {
-          url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+          url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
           success: function ( data ) {
             var post = data.shift(); // The data is an array of posts. Grab the first one.
             $( '#quote-title' ).text( post.title );
