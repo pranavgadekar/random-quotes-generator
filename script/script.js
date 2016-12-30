1e06e6b98dfcbc172ed05d425b288387989441c1
pranavgadekar <ppgadekar.92@gmail.com>
2016-12-30 18:20:50 -0500

    Added animations for content

--- script/script.js
+++ script/script.js
@@ -5,8 +5,10 @@ jQuery( function( $ ) {
           url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
           success: function ( data ) {
             var post = data.shift(); // The data is an array of posts. Grab the first one.
-            $( '#quote-title' ).text( post.title );
+            $('#slide-down-animation').hide();
             $( '#quote-content' ).html(post.content );
+            $( '#quote-title' ).text( post.title );
+            $("#slide-down-animation").show( "slide");
           },
           cache: false
         } );
